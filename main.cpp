629b3207ff991ac9ff6a76f27299c74925934de4
liza <shetinina.l2000@gmail.com>
2024-05-09 18:09:35 +0400

    второй комит

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QtCore>
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char **argv [])
 {
  QApplication app (argc, argv);
+ qDebug()<<"START PROGRAM";  
  return app.exec();
 }
