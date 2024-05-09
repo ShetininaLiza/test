c0205f139f389debf1020422d5d07fcc1a007d81
liza <shetinina.l2000@gmail.com>
2024-05-09 18:52:39 +0400

    Проверка

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <QtCore>
+#include <QDialog>
 #include <QApplication>
 
 int main(int argc, char **argv [])
 {
  QApplication app (argc, argv);
+QDialog *dialog = new QDialog();
+dialog->setText("111");
+dialog->show();
  return app.exec();
 }
